95fafc9bfe9c840baf40fe305cfd36dff71f3555
MMomcilovic <61472289+MMomcilovic@users.noreply.github.com>
2024-08-25 12:50:05 +0200

    feat: project initialization with cicd (#1)

---
+++ src/test/java/com/devops/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.devops.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
